0010d06bbfe074a28d1fd3676d0050d3148f7ea5
bryanmoon1991 <bryanmoon1991@gmail.com>
2021-03-02 14:17:37 -0500

    fixed login endpoint for deployed version

--- src/App.js
+++ src/App.js
@@ -7,80 +7,81 @@ import { Menu, Button } from 'semantic-ui-react';
 import './App.css';
 
 const App = ({ user, clearResults, history, handleLogout }) => {
-  let [activeItem, setActiveItem] = useState('home');
+	let [activeItem, setActiveItem] = useState('home');
 
-  const handleItemClick = (e) => {
-    let { name } = e.target;
-    setActiveItem(name);
-  };
+	const handleItemClick = (e) => {
+		let { name } = e.target;
+		setActiveItem(name);
+	};
 
-  const logOut = () => {
-    localStorage.clear();
-    handleLogout();
-    history.push('/');
-    window.location.reload(true);
-  };
+	const logOut = () => {
+		localStorage.clear();
+		handleLogout();
+		history.push('/');
+		window.location.reload(true);
+	};
 
-  return (
-    <>
-      {user ? (
-        <>
-          <Menu color="grey" size="small">
-            <Menu.Item
-              as={Link}
-              to={`/users/${user.id}`}
-              name="home"
-              active={activeItem === 'home'}
-              onClick={(e) => {
-                handleItemClick(e);
-                clearResults();
-              }}
-            />
-            <Menu.Item
-              as={Link}
-              to={`/users/${user.id}/playlists`}
-              name="playlists"
-              active={activeItem === 'playlists'}
-              onClick={(e) => {
-                handleItemClick(e);
-                clearResults();
-              }}
-            />
+	return (
+		<>
+			{user ? (
+				<>
+					<Menu color="grey" size="small">
+						<Menu.Item
+							as={Link}
+							to={`/users/${user.id}`}
+							name="home"
+							active={activeItem === 'home'}
+							onClick={(e) => {
+								handleItemClick(e);
+								clearResults();
+							}}
+						/>
+						<Menu.Item
+							as={Link}
+							to={`/users/${user.id}/playlists`}
+							name="playlists"
+							active={activeItem === 'playlists'}
+							onClick={(e) => {
+								handleItemClick(e);
+								clearResults();
+							}}
+						/>
 
-            <Menu.Item position="right">
-              <Button onClick={() => logOut()} primary>
-                Log Out
-              </Button>
-            </Menu.Item>
-          </Menu>
-        </>
-      ) : (
-        <>
-          <div className="login">
-            <div className="login-items">
-              <Button
-                className="login-button"
-                color="green"
-                circular
-                icon="spotify"
-                size="massive"
-                as="a"
-                href="http://localhost:3000/api/v1/login"
-              />
-              <h2>perfect playlist</h2>
-            </div>
-          </div>
-        </>
-      )}
-      <Route path="/users" component={UsersContainer} />
-    </>
-  );
+						<Menu.Item position="right">
+							<Button onClick={() => logOut()} primary>
+								Log Out
+							</Button>
+						</Menu.Item>
+					</Menu>
+				</>
+			) : (
+				<>
+					<div className="login">
+						<div className="login-items">
+							<Button
+								className="login-button"
+								color="green"
+								circular
+								icon="spotify"
+								size="massive"
+								as="a"
+								// href="http://localhost:3000/api/v1/login"
+								href="https://playlister-app-api.herokuapp.com/api/v1/login"
+							/>
+							<h2>perfect playlist</h2>
+						</div>
+					</div>
+				</>
+			)}
+			<Route path="/users" component={UsersContainer} />
+		</>
+	);
 };
 
 const msp = (state) => {
-  return {
-    user: state.user,
-  };
+	return {
+		user: state.user,
+	};
 };
 
 export default withRouter(connect(msp, { clearResults, handleLogout })(App));
--- src/spotify/actions.js
+++
@@ -1,1 +0,0 @@
-export const followArtist = (artist, spotifyApi) => {};
